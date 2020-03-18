a349812de04392921655bcb1a0b55e7acc9a4ef6
Dani <daniel.puente@alumni.mondragon.edu>
2020-03-18 10:18:28 +0100

    Actualiza el programa de R

--- Analisis por ciudad.R
+++ Analisis por ciudad.R
@@ -7,9 +7,11 @@ print(datos$CCAA)
 pais_vasco<-datos[datos$CCAA=="País Vasco",]
 cataluña<-datos[datos$CCAA=="Cataluña",]
 madrid<-datos[datos$CCAA=="Madrid",]
+andalucia<-datos[datos$CCAA=="Andalucia",]
 
 plot(t(pais_vasco[-c(1:2)]),type="l",col="red")
 par(new=T)
 plot(t(cataluña[-c(1:2)]),type="l",col="green")
 par(new=T)
 plot(t(madrid[-c(1:2)]),type="l",col="blue")
+plot(t(andalucia[-c(1:2)]),type="l",col="blue")
